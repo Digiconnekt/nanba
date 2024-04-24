d4e871f5f2d3b9b9a96521c7b01bb7ae206ae96e
Rohan Daddekar <rohandaddekar9@gmail.com>
2024-04-24 11:21:22 +0530

    updated lets get started

--- src/pages/Home/partials/LetsGetStarted.jsx
+++ src/pages/Home/partials/LetsGetStarted.jsx
@@ -11,7 +11,7 @@ const LetsGetStarted = () => {
       <section className="bg-[#E6F2F1] overflow-hidden">
         <div className="relative mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-20 lg:px-8 lg:py-24">
           <motion.div
-            className="max-w-lg"
+            className="md:max-w-md text-center md:text-left"
             variants={fadeInLeft()}
             initial="hidden"
             whileInView="visible"
@@ -27,6 +27,7 @@ const LetsGetStarted = () => {
             <img
               src="../../../../images/home/let-get-started.png"
               alt="Contact Us"
+              className="w-[550px] lg:w-full"
             />
           </figure>
         </div>
